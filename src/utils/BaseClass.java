a4c92c47b72493d92d794e9b10b1567e23a2e21f
smirza <shahana23mirza@gmail.com>
2024-08-20 17:58:21 -0400

    My second commit

---
+++ src/utils/BaseClass.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class BaseClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
--- src/utils/Constantss.java
+++ src/utils/Constantss.java
@@ -5,8 +5,11 @@ public class Constantss {
 	public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")
 			+ "/configs/configuration.properties";
 	
+
+	
 	public static final int IMPLICIT_WAIT_TIME = 10;
 	
 	public static final int EXPLICIT_WAIT_TIME = 30;
-
+	
+	
 }
